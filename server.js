f68ed90c124a90640df897c021d351b1442aa047
Omar Hassan <101973900+KhalidHassan218@users.noreply.github.com>
2025-11-29 16:41:49 +0400

    shit

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const fs = require('fs'); // Use synchronous version for initial setup
 //   serviceAccount = require("./serviceAccountKey.json");
 //   console.log("✅ Service account loaded from local file.");
 // }
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const path = require('path');
+const serviceAccount = require(path.resolve(process.env.GOOGLE_APPLICATION_CREDENTIALS));
 
 const { v4: uuidv4 } = require("uuid");
 const admin = require("firebase-admin");
