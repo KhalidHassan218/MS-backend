0a632efb3cb53a9c785eb6ceeddac75e481364b8
Omar Hassan <sergio_eersel@hotmail.com>
2026-01-15 23:32:12 +0400

    miss escapeHtml

--- templates/licence/generateLicenceHTML.js
+++ templates/licence/generateLicenceHTML.js
@@ -1,5 +1,12 @@
 import templates from "./templates.js";
-
+function escapeHtml(str) {
+  return String(str || "")
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
 function generateLicenceHTML(
     licenseData,
     companyCountryCode = "EN",
